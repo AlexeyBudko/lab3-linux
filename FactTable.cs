0923750c432ff0ea58ebc1c434a51fb4ce2a532a
AlexeyBudko <lesha_11@bk.ru>
2013-11-29 22:36:28 +0300

    first commit

---
+++ FactTable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace вывод_данных_из_бд
+{
+    class FactTable
+    {
+        private String name;
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+       
+        private String primaryKey;
+        public String PrimaryKey
+        {
+            get { return primaryKey; }
+            set { primaryKey = value; }
+        }
+
+        private Dictionary<String, Tuple<int, String>> fields;
+        public Dictionary<String, Tuple<int, String>> Fields
+        {
+            get { return fields; }
+        }
+
+        public FactTable()
+        {
+            this.fields = new Dictionary<String, Tuple<int, String>>();
+        }
+    }
+}
